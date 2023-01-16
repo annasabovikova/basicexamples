815a53642eed3f6a458f2d55fa0cfb6ec2da171d
DELL <annasabovikova@gmail.com>
2023-01-16 16:46:49 +0100

    Added all files

---
+++ basicexamples/exercises/Circle.java
@@ -0,0 +1,11 @@
+package basicexamples.exercises;
+
+public class Circle {
+    public static void main(String[] args) {
+        //Program Circle bude od používateľa vyžadovať vstupnú hodnotu a to polomer kruhu, ktorý
+        //bude v centimetroch. Výsledkom bude obsah a obvod kruhu, ktorý vypíše používateľovi do
+        //“konzoly”. Program pôjde dovtedy, kým nezadáme slovo: koniec.
+
+
+    }
+}
